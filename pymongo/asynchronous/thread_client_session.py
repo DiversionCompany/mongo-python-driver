fa3bb469087081e8bcfe10d5a2f0ced93d3b3c5c
Avihad Menahem <avihad@diversion.dev>
2025-02-19 14:10:08 +0200

    Add _IS_SYNC

--- pymongo/asynchronous/thread_client_session.py
+++ pymongo/asynchronous/thread_client_session.py
@@ -2,15 +2,16 @@ import threading
 from contextlib import contextmanager
 from typing import Generator, Optional
 
-from pymongo.synchronous.client_session import ClientSession
+from pymongo.asynchronous.client_session import AsyncClientSession
 
-_IS_SYNC = True
+_IS_SYNC = False
 
 _mongo_thread_local_storage = threading.local()
 
+# The async client is not supported, we didn't implemented it yet
 
 @contextmanager
-def thread_client_session(session: ClientSession) -> Generator:
+def thread_client_session(session: AsyncClientSession) -> Generator:
     """
     Set the session as the current active session for the thread during the context of this generator.
     """
@@ -21,5 +22,5 @@ def thread_client_session(session: ClientSession) -> Generator:
         _mongo_thread_local_storage.session = None
 
 
-def get_thread_client_session() -> Optional[ClientSession]:
+def get_thread_client_session() -> Optional[AsyncClientSession]:
     return getattr(_mongo_thread_local_storage, "session", None)
